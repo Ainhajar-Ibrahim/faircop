82dd64e254ac7e2f1374ab8e101f0cc6d2efa2b9
AINHAJAR IBRAHIM <ibrahimainhajar1@gmail.com>
2022-09-13 11:57:45 +0200

    up to date

--- src/main/java/com/emse/spring/faircorp/Application.java
+++ src/main/java/com/emse/spring/faircorp/Application.java
@@ -1,4 +1,4 @@
-package com.emse.spring.faircorp.jar;
+package com.emse.spring.faircorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/emse/spring/faircorp/hello/DummyUserService.java
@@ -0,0 +1,21 @@
+package com.emse.spring.faircorp.hello;
+
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class DummyUserService implements UserService {
+    private String[] names={"Elodie","Charles"};
+    private GreetingService greetingService;
+
+    public DummyUserService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+
+    }
+    @Override
+    public void greetAll() {
+    this.greetingService.greet(names[0]);
+        this.greetingService.greet(names[1]);
+    }
+}
